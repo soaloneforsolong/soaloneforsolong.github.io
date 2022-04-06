76a9153032b7fba8f92d0698c9031ba06fd48b30
soaloneforsolong <102030432+soaloneforsolong@users.noreply.github.com>
2022-04-06 23:00:57 +0300

    genshin

---
+++ script.js
@@ -0,0 +1,24 @@
+let btn_prev = document.querySelector('#gallery .buttons .prev');
+let btn_next = document.querySelector('#gallery .buttons .next');
+
+let images = document.querySelectorAll('#gallery .carusel__photos img');
+
+let i = 0;
+
+btn_next.onclick = function(){
+    images[i].style.opacity = 0;
+    i++
+    if(i >= images.length){
+    i = 0;
+    }
+    images[i].style.opacity = 1;
+}
+
+btn_prev.onclick = function(){
+    images[i].style.opacity = 0;
+    i = i - 1;
+    if(i < 0){
+        i = images.length - 1;
+    }
+    images[i].style.opacity = 1;
+}
