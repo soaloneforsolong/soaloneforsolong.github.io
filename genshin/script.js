8198a644c4b0250291c95dbc2d53bb3b7dd76846
soaloneforsolong <soaloneforsolong@ya.ru>
2022-04-12 14:28:51 +0300

    burger

--- genshin/script.js
+++ genshin/script.js
@@ -22,3 +22,16 @@ btn_prev.onclick = function(){
     }
     images[i].style.opacity = 1;
 }
+
+const hamburger = document.querySelector(".hamburger");
+const menuList = document.querySelector(".menu__list");
+
+hamburger.addEventListener ("click", () => {
+    hamburger.classList.toggle("active");
+    menuList.classList.toggle("active");
+});
+
+document.querySelectorAll(".menu__list").forEach(n => n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    menuList.classList.remove("active");
+}))
